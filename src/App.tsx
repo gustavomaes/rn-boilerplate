69f353177db29a6f9aad621ac781b89087f63757
Gustavo <gustavomaescd@gmail.com>
2020-11-04 17:06:35 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Button from 'component/Button'
+import React from 'react'
+import { Text } from 'react-native'
+
+const App = () => {
+  return (
+    <>
+      <Text>Template</Text>
+      <Button />
+    </>
+  )
+}
+
+export default App
