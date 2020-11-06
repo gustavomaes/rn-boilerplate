19adcc7a4d3e542b94571095661702c7f2aa04d6
Gustavo <gustavomaescd@gmail.com>
2020-11-06 14:07:16 -0300

    feat: add iOS splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import 'react-native-gesture-handler'
-import React from 'react'
+import React, { useEffect } from 'react'
+import SplashScreen from 'react-native-splash-screen'
 import { ThemeProvider } from 'styled-components'
 
 import theme from 'styles/theme'
 import Navigation from 'routes/Navigation'
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <Navigation />
